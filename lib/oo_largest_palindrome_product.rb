514413c4ed9f3d0a07b75cdc42373c91fada2e05
George Toothman <george.toothman@flatironschool.com>
2015-10-13 14:08:44 -0400

    add sdraft to solution

--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,25 @@
 # Implement your object-oriented solution here!
+require 'pry'
+
+class LargestPalindromeProduct
+  attr_reader :num_set
+  def initialize 
+    @num_set = 301 * 893
+    self.find_combos_one(num_set)
+  end
+
+  def find_combos_one(num_set)
+    tmp = num_set.to_s.split('')
+    new_array = Array.new
+    tmp.map do |int|
+      new_array << int.to_i
+    end
+    combos = new_array.combination(6).to_a 
+    combos[0].each do |num|
+      binding.pry  
+    end
+
+  end
+  
+  test = LargestPalindromeProduct.new
+end
